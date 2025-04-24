594adfbd067d4fad68b5fbbad04441015bd04f44
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-04-24 10:02:30 -0400

    Create a Nimbus like view of our public website.
    I was given direction from Msgr. Winslow that he always wanted Nimbus/Crypta
    to look similar to the look and feel to the public website while being
    different enough to know you're on a different page. I've completely
    changed the base.html to make the look and feel almost exactly like
    the public website while feeling more cloud or nimbus like.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,7 +6,7 @@ handler404 = views.view_404
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path('test-404', views.view_404, name='test_404'),
     
 ]
