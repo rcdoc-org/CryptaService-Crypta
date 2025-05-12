24524ff2588b04b035c2002e5eb08b5561fcd1ed
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-05-12 19:43:27 -0400

    Starting of nav usage for right panel control.

---
+++ backend/api/static/js/details_page.js
@@ -0,0 +1,36 @@
+(function () {
+    const links = document.querySelectorAll('.aside-panel .nav-link');
+    const panels = document.querySelectorAll('.detail-section');
+
+    function showPanel(id) {
+        panels.forEach(p => p.id === id
+            ? p.classList.remove('d-none')
+            : p.classList.add   ('d-none'));
+    }
+
+    function linking() {
+        links.forEach(link =>{
+            link.addEventListener('click', e => {
+                e.preventDefault();
+                // toggle active class on links
+                links.forEach(l => l.classList.remove('active'));
+                link.classList.add('active');
+
+                // show corresponding panel
+                const target = link.CDATA_SECTION_NODE.target;
+                showPanel(target);
+            });
+        });
+    }
+
+    function init() {
+        linking();
+    }
+
+    // run init() once the DOM is ready
+    if(document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', init);
+    } else {
+        init();
+    }
+})();
