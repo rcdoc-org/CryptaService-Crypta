fd1b6fc48413a1da6cb04f193e2ddbb5b65c9282
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-06-12 11:05:33 -0400

    Recreate of Changelog.

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 
-const Card = ({ title, children, className = '' }) => {
-    <div className={`card mb-4 border-0 rounded-4 shadow-sm ${className}`}>
+const Card = ({ 
+    title, 
+    children, 
+    className = '', 
+    headerTag = 'small' 
+}) => {
+    const HeaderTag = headerTag
+
+    return (
+        <div className={`card mb-4 border-0 rounded-4 shadow-sm ${className}`}>
         <div className="card-body">
-            {title && <small className="text-muted">{title}</small>}
+            {title && headerTag === 'small' && (
+                <HeaderTag className="text-muted">
+                    {title}
+                </HeaderTag>
+            )}
+            {title && (
+                <HeaderTag className="text-center">
+                    {title}
+                </HeaderTag>
+            )}
             <div className="mt-2">
                 {children}
             </div>
         </div>
     </div>
+    )
+    
 };
 
 export default Card;
