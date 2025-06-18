a8ee7fde1ad1848de660dca63a6a24e68c83810b
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-06-18 12:14:10 -0400

    Creating gateway service.

---
+++ services/gateway/server.py
@@ -0,0 +1,24 @@
+import os, json
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+
+server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/docs"
+
+mongo = PyMongo(server)
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
+    
+@server.route("/validate", methods=["POST"])
+def validate():
+    access, err = validate.token(request)
