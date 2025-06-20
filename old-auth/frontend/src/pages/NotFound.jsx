b92f388b34436eb7e36486674414cdbe1c4c745a
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-06-20 09:25:00 -0400

    Moved old frontend for auth into repo for codex to see.

---
+++ old-auth/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,19 @@
+import Home from "./Home";
+
+function NotFound() {
+    return <div>
+        <h1>404 - Page not Found</h1>
+        <h3>
+            Do Not Be Alarmed! Jesus is not there,
+            <br/>
+            but we are here to help you find Him.
+        </h3>
+        <h3 style={{ fontStyle: 'italic'}}>
+            “They have taken the Lord out of the tomb, and we don’t know where they have put him!”
+            <br/>
+            John 20:2
+        </h3>
+    </div>
+}
+
+export default NotFound;
