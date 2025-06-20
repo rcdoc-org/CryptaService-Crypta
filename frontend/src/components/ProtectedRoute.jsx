73a23ed6ded6fa7936c2aaee31b42939ee7fe4fd
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-06-20 12:15:02 +0000

    Editted the ProtectedRoute.jsx import path.

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import api from "../api/api";
-import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../../constants';
 import { useState, useEffect } from 'react';
 
 function ProtectedRoute({ children }) {
