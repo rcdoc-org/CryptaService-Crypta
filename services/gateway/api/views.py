7796bcaa5e377c833349cda9d6e92f9eb3eb4249
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-06-25 08:10:30 -0400

    started process of building url routing to auth service from gateway.

--- services/gateway/api/views.py
+++ services/gateway/api/views.py
@@ -1,3 +1,6 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+def createUserView_v1():
+    pass
--- services/gateway/gateway/urls.py
+++ services/gateway/gateway/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import CreateUserView_v1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', createUserView_v1.as_view(), name='register'),
 ]
