d68ac2ac0c010598ea0472acd92239382d3bc8d3
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-06-26 12:42:16 -0400

    Added create function for the registration.

--- services/auth/api/serializers.py
+++ services/auth/api/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from . import models
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
     class Meta:
         model = models.User
         fields = '__all__'
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        validated_data['password_hash'] = make_password(password)
+        return super().create(validated_data)
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
