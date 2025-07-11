adaee047fa8705b424875ba6b2e157858ebb20b6
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-07-11 14:15:01 -0400

    Made it easy to swap between local and dev sql.

--- services/auth/auth/settings.py
+++ services/auth/auth/settings.py
@@ -145,6 +145,10 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
         'ENGINE': 'mssql',
         'NAME': os.getenv('DB_NAME'),
