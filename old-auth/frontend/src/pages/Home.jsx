77b070e0be6afc0fe5204251f48534da2e377b85
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-07-16 18:52:18 -0400

    Repo clean up of unused portions.

--- old-auth/frontend/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import { useState, useEffect } from 'react';
-import api from "../api";
-
-function Home() {
-    return <div>Home</div>
-}
-
-export default Home;
--- old-auth/frontend/src/pages/Login.jsx
+++
@@ -1,7 +0,0 @@
-import Form from '../components/Form';
-
-function Login() {
-    return <Form route='/api/v1/token/' method='login' />;
-}
-
-export default Login;
