c972e108d5f6f43914981e5f8594f51800931dda
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-07-17 18:12:57 -0400

    Created a unit test for permissions.

---
+++ services/crypta/api/tests/test_permission_filters.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from api.models import Person
+from api.views import _apply_permission_filters
+
+class ApplyPermissionFiltersTests(TestCase):
+    def test_permission_on_related_resource(self):
+        person = Person.objects.create(
+            personType='lay',
+            name_first='Jane',
+            name_last='Doe',
+        )
+        perms = [
+            {
+                'resource': 'person_email',
+                'filters': {},
+            }
+        ]
+        qs = Person.objects.all()
+        filtered = _apply_permission_filters(qs, perms, 'person')
+        self.assertQuerysetEqual(filtered, [person], transform=lambda x: x)
