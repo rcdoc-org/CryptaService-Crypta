625173537335764b952c292ef3dcbe42ed29bff2
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-07-18 12:45:46 -0400

    Added a proper import of bootstrap.

--- frontend/src/pages/Database.jsx
+++ frontend/src/pages/Database.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import * as bootstrap from 'bootstrap';
 import * as XLSX from 'xlsx';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
